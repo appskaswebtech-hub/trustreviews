4fecc4f5c5f71d9e6bd06f071a4a5c7890fd4579
balpreetkaswebtech-rgb <balpreet.kaswebtech@gmail.com>
2026-04-24 10:07:06 +0530

    remove-url

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -23,9 +23,9 @@ export default function App() {
       <PolarisProvider i18n={enTranslations}>
 
         <s-app-nav>
-          <s-link href="/app">Home</s-link>
+   
           <s-link href="/app/review">Reviews</s-link>
-          <s-link href="/app/additional">Additional page</s-link>
+   
         </s-app-nav>
 
         <Outlet />
